4b53bc3973b61ccb678911b2c0cadf80f2836756
GauravDhanuka <gauravdhanuka2007@gmail.com>
2018-06-10 16:04:00 +0530

    Added MYSQL file to add mock data

---
+++ mock-data.sql
@@ -0,0 +1,24 @@
+DELETE FROM exercise_stats;
+DELETE FROM assessment_stats;
+DELETE FROM exercise;
+DELETE FROM topic;
+DELETE FROM template;
+DELETE FROM assessment;
+DELETE FROM course;
+
+INSERT INTO course VALUES (1, 'Alphabet'), (2, 'Number');
+
+INSERT INTO assessment (id, threshold, course_id) VALUES (1, 30.0, 1), (2, 30.0, 2);
+
+INSERT INTO template (id, content_type, content_prefix, content_style, name) VALUES (1, 'Alphabet', 'Identify', 'Bold', 'bold alphabet'), (2, 'Number', 'Identify', 'Bold', 'bold number');
+
+INSERT INTO topic (id, name, course_id) VALUES (1, 'Capslock', 1), (2, 'Even', 2);
+
+INSERT INTO exercise (id, content, content_type, topic_id, template_id) VALUES (1, 'A', 'ALPHABET', 1, 1), (2, '3', 'NUMBER', 2, 2);
+
+INSERT INTO assessment_stats (id, score, user_id, assessment_id) VALUES (1, 40.0, 4, 1), (2, 20.0, 4, 2);
+
+INSERT INTO exercise_stats (id, status, user_id, exercise_id) VALUES (1, 1, 4, 1), (2, 0, 4, 2);
+
+
+
